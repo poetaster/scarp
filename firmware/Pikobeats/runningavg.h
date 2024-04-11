9887c42d04accb6d8a79b7f1162699eb3a378264
Mark Washeim <blueprint@poetaster.de>
2024-04-11 15:31:05 +0200

    Add: add running avg. not used currently

---
+++ firmware/Pikobeats/runningavg.h
@@ -0,0 +1,42 @@
+#ifndef RAVG_H
+#define RAVG_H
+#include <Arduino.h>
+
+class RunningAverage {
+  uint16_t vals[32];
+  bool hasnum[32];
+  uint8_t len;
+  uint8_t i;
+  uint16_t current;
+
+ public:
+  static RunningAverage* Instance();
+  
+  void Init(uint8_t len_) {
+    len = len_;
+    i = 0;
+  }
+
+  void Update(uint16_t v) {
+    if (i >= len) {
+      i = 0;
+    }
+    vals[i] = v;
+    hasnum[i] = true;
+    double count = 0;
+    double total = 0;
+    for (uint8_t j = 0; j < len; j++) {
+      if (hasnum[j]) {
+        total += ((double)vals[j]);
+        count++;
+      }
+    }
+    total = round(total / count);
+    i++;
+    current = ((uint16_t)total);
+  }
+
+  uint16_t Value() { return current; }
+};
+
+#endif
